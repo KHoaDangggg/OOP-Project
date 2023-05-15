c7eec15aabb2d0718113620984c66aec6a3f880b
KHoaDangggg <khoa.dp210486@sis.hust.edu.vn>
2023-05-15 14:37:17 +0700

    Initialize project

---
+++ src/main/java/CrawlData/CrawTrieuDai/King.java
@@ -0,0 +1,28 @@
+package CrawlData.CrawTrieuDai;
+
+public class King {
+    public String name;
+    public String time;
+
+    public King(String name, String time) {
+        this.name = name;
+        this.time = time;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+}
---
+++ src/main/java/CrawlData/CrawTrieuDai/war.java
@@ -0,0 +1,40 @@
+package CrawlData.CrawTrieuDai;
+
+public class war {
+    public String time;
+    public String name;
+    public String desc;
+    public String trieu_dai;
+    public String phe_minh;
+    public String phe_dich;
+    public String result;
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setTrieu_dai(String trieu_dai) {
+        this.trieu_dai = trieu_dai;
+    }
+
+    public void setPhe_minh(String phe_minh) {
+        this.phe_minh = phe_minh;
+    }
+
+    public void setPhe_dich(String phe_dich) {
+        this.phe_dich = phe_dich;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
