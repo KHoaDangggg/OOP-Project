b9638656e4baa614b9cfaa1549a5573254853142
1plus1equal3 <115053943+1plus1equal3@users.noreply.github.com>
2023-06-06 14:55:07 +0700

    Update crawl from multiple links from wiki

---
+++ src/main/java/CrawlLeHoi/LeHoi_Nguon_04/LeHoiWiki.java
@@ -0,0 +1,40 @@
+package CrawlLeHoi.LeHoi_Nguon_04;
+
+import java.util.HashMap;
+
+public class LeHoiWiki {
+    private String ten;
+
+    private String mieuTa;
+    private HashMap<String, String> dacDiemLeHoi;
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public HashMap<String, String> getDacDiemLeHoi() {
+        return dacDiemLeHoi;
+    }
+
+    public void setDacDiemLeHoi(HashMap<String, String> dacDiemLeHoi) {
+        this.dacDiemLeHoi = dacDiemLeHoi;
+    }
+
+    public String getMieuTa() {
+        return mieuTa;
+    }
+
+    public void setMieuTa(String mieuTa) {
+        this.mieuTa = mieuTa;
+    }
+
+    public LeHoiWiki(String ten, String mieuTa, HashMap<String, String> dacDiemLeHoi) {
+        this.ten = ten;
+        this.mieuTa = mieuTa;
+        this.dacDiemLeHoi = dacDiemLeHoi;
+    }
+}
