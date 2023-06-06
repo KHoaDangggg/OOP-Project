ed1a929247dbbf0da6952d47f96d7a0bf903865f
1plus1equal3 <115053943+1plus1equal3@users.noreply.github.com>
2023-06-06 14:14:09 +0700

    Update

--- src/main/java/CrawlLeHoi/CrawLeHoiMain.java
+++ src/main/java/CrawlLeHoi/CrawLeHoiMain.java
@@ -2,13 +2,17 @@ package CrawlLeHoi;
 
 import CrawlLeHoi.LeHoi_Nguon_01.CrawlLeHoiNguon01;
 import CrawlLeHoi.LeHoi_Nguon_02.CrawlLeHoiNguon02;
+import CrawlLeHoi.LeHoi_Nguon_03.CrawlLeHoiNguon03;
 
 public class CrawLeHoiMain {
 
     private static final CrawlLeHoiNguon01 nguon01 = new CrawlLeHoiNguon01();
     private static final CrawlLeHoiNguon02 nguon02 = new CrawlLeHoiNguon02();
+    private static final CrawlLeHoiNguon03 nguon03 = new CrawlLeHoiNguon03();
+
     public static void main(String[] args) {
         nguon01.crawlFromWiki();
         nguon02.wolCrawl();
+        nguon03.crawlMultipleLinks();
     }
 }
