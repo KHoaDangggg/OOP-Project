b49d509ab3dcff0e55640c739d20a00da0f8d7ec
hainghuu19 <nguyenhuuhai7a@gmail.com>
2023-06-07 14:53:02 +0700

    anh hung va danh nhan thoi dinh

---
+++ src/main/java/CrawlData/CrawlTrieuDai/CrawlAnhHung/AnhHung.java
@@ -0,0 +1,33 @@
+package CrawlData.CrawlTrieuDai.CrawlAnhHung;
+
+public class AnhHung {
+    private String hoVaTen;
+    private String tuLieu;
+    private String tomTat;
+    private String queQuan;
+
+
+    public String getHoVaTen() {
+        return hoVaTen;
+    }
+
+    public String gettuLieu() {
+        return tuLieu;
+    }
+
+    public String gettomTat() {
+        return tomTat;
+    }
+
+    public String getQueQuan() {
+        return queQuan;
+    }
+
+
+    public AnhHung(String hoVaTen, String tuLieu, String queQuan, String tomTat) {
+        this.hoVaTen = hoVaTen;
+        this.tuLieu = tuLieu;
+        this.tomTat = tomTat;
+        this.queQuan = queQuan;
+    }
+}
