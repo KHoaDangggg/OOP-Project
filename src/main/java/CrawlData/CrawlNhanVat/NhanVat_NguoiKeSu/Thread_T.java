76a7b7e2f9132496d572b184e0dc4d329ef84966
1plus1equal3 <115053943+1plus1equal3@users.noreply.github.com>
2023-06-09 12:46:01 +0700

    Add NhanVatLichSu.json include 1000 nhan vat lich su from Nguoikesu.com

---
+++ src/main/java/CrawlData/CrawlNhanVat/NhanVat_NguoiKeSu/Thread_T.java
@@ -0,0 +1,35 @@
+package CrawlData.CrawlNhanVat.NhanVat_NguoiKeSu;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Thread_T implements Runnable{
+
+    private String url;
+
+    @Override
+    public void run() {
+        Document doc;
+        try {
+            doc = Jsoup.connect(url)
+                    .get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Elements names = doc.select("h2[itemprop=name]").select("a");
+        for (org.jsoup.nodes.Element name : names) {
+            String url = name.attr("href");
+            url = "https://nguoikesu.com" + url;
+            //System.out.println(url);
+            Main.listlink.add(url);
+        }
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
