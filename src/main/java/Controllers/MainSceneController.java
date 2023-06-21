f9f95d3dd31a80b81c98d29570ffa96d9466f28a
1plus1equal3 <115053943+1plus1equal3@users.noreply.github.com>
2023-06-21 18:19:48 +0700

    Update

--- src/main/java/Controllers/MainSceneController.java
+++ src/main/java/Controllers/MainSceneController.java
@@ -3,15 +3,18 @@ package Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class MainSceneController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainSceneController implements Initializable {
 
     @FXML
     private Button btnDynasty;
@@ -46,7 +49,7 @@ public class MainSceneController {
             renderNewScence("Sự Kiện");
         }
         if (event.getSource() == btnFigures) {
-            renderNewScence("Nhân vật lịch sử");
+            renderNewScence("Nhân Vật Lịch Sử");
         }
         if (event.getSource() == btnExit) {
             Stage stage = (Stage) btnExit.getScene().getWindow();
@@ -55,16 +58,51 @@ public class MainSceneController {
     }
     private void renderNewScence(String buttonName) {
         try {
-            FXMLLoader newLoader = new FXMLLoader(getClass().getResource("../fxml/SearchScene.fxml"));
-            Parent root = newLoader.load();
-            Scene newScene = new Scene(root);
-            Stage newStage = new Stage();
-            newStage.setScene(newScene);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/SearchScene.fxml"));
+            Parent root = loader.load();
             ChoiceBox<String> choiceBox = (ChoiceBox<String>) root.lookup("#choiceBox");
             choiceBox.setValue(buttonName);
-            newStage.show();
+            Scene currentScene = btnDynasty.getScene();
+            Button btn = (Button) root.lookup("#exitBtn");
+            currentScene.setRoot(root);
+            SearchSceneController controller = loader.getController();
+            controller.initialize(getClass().getResource("../fxml/SearchScene.fxml"), null);
+            btn.setOnAction(event -> {
+                try {
+                    FXMLLoader loader1 = new FXMLLoader(getClass().getResource("../fxml/MainScene.fxml"));
+                    Parent root1 = loader1.load();
+                    currentScene.setRoot(root1);
+                    MainSceneController controller1 = loader1.getController();
+                    controller1.initialize(getClass().getResource("../fxml/MainScene.fxml"), null);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+//    private void renderNewScence(String buttonName) {
+//        try {
+//            FXMLLoader newLoader = new FXMLLoader(getClass().getResource("../fxml/SearchScene.fxml"));
+//            Parent root = newLoader.load();
+//            SearchSceneController controller = newLoader.getController();
+//            Scene currentScene = btnExit.getScene();
+//            Stage stage = (Stage) currentScene.getWindow();
+//            controller.setScene(currentScene);
+//            Scene newScene = new Scene(root);
+//            stage.setScene(newScene);
+//            ChoiceBox<String> choiceBox = (ChoiceBox<String>) root.lookup("#choiceBox");
+//            choiceBox.setValue(buttonName);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
